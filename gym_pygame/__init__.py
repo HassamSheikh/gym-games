4b7ce01570d068b3efd0041b93e68e068573347b
Hassam Ullah Sheikh <hassamsheikh1@gmail.com>
2020-03-07 14:18:38 -0500

    Update __init__.py

--- gym_pygame/__init__.py
+++ gym_pygame/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 for game in ['Catcher', 'FlappyBird', 'Pixelcopter', 'PuckWorld', 'Pong']:
   register(
     id='{}-PLE-v0'.format(game),
-    entry_point=f'gym_pygame.envs:{game}Env'
+    entry_point="gym_pygame.envs:{}Env".format(game)
 )
