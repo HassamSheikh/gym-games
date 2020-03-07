69af00205316b46b8c17f81a74da4b0d8e305f20
Hassam Ullah Sheikh <hassamsheikh1@gmail.com>
2020-03-07 14:24:37 -0500

    Update __init__.py

--- gym_minatar/__init__.py
+++ gym_minatar/__init__.py
@@ -3,5 +3,5 @@ from gym.envs.registration import register
 for game in ['Asterix', 'Breakout', 'Freeway', 'Seaquest', 'Space_invaders']:
   register(
     id='{}-MinAtar-v0'.format(game),
-    entry_point=f'gym_minatar.envs:{game}Env'
+    entry_point="gym_minatar.envs:{}".format(game)
 )
